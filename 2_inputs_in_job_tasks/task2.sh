4c27980cabc5e85efe373ff3751370f55b260be9
Devika Beniwal <dbeniwal1@hawk.iit.edu>
2017-06-16 11:59:54 -0500

    Update task2.sh

--- 2_inputs_in_job_tasks/task2.sh
+++ 2_inputs_in_job_tasks/task2.sh
@@ -3,9 +3,11 @@
 
 set -e
 
-# task script is in resource-tutorial/10_job_inputs/ folder
+# task script is in res1/2_inputs_in_job_tasks/ folder
 # application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
 # $GOPATH is gopath/ folder
+echo $(pwd)/gopath
+
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
 cd gopath/src/github.com/cloudfoundry-community/simple-go-web-app/
 
