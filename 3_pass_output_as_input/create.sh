73e9f169fbe1b8bcc6cbf6ab742ef08247db4de0
Devika Beniwal <dbeniwal1@hawk.iit.edu>
2017-06-16 14:11:48 -0500

    Add files via upload

---
+++ 3_pass_output_as_input/create.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir some_files
+echo "file1" > some_files/file1
+echo "file2" > some_files/file2
+echo "file3" > some_files/file3
+echo "file4" > some_files/file4
+
+
+ls some_files/*
